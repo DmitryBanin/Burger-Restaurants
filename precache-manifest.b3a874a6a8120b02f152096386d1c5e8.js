cb88909778a424336891f7a8c956ddd35b972b8e
DmitryBanin <banindm@mail.ru>
2023-01-03 08:45:14 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ precache-manifest.b3a874a6a8120b02f152096386d1c5e8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0942fb89b686ef00fd5eaf504ed0a7c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4f0cdd1abe20d4143c0",
+    "url": "/static/css/main.be469bcf.chunk.css"
+  },
+  {
+    "revision": "c4e046b743199f05e5de",
+    "url": "/static/js/2.9516f918.chunk.js"
+  },
+  {
+    "revision": "e4f0cdd1abe20d4143c0",
+    "url": "/static/js/main.c408d127.chunk.js"
+  },
+  {
+    "revision": "fa27f71bd95d5ca81881",
+    "url": "/static/js/runtime-main.cf8cab74.js"
+  },
+  {
+    "revision": "385ae8f1201379005ad5f888db39e059",
+    "url": "/static/media/burger-logo-4.385ae8f1.jpg"
+  },
+  {
+    "revision": "1d4aace8ece50372c4a71e8334f161a1",
+    "url": "/static/media/down.1d4aace8.svg"
+  },
+  {
+    "revision": "551b5c6b9714b6eafb5739b138485841",
+    "url": "/static/media/up.551b5c6b.svg"
+  }
+]);
